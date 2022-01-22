b799d243d7ac4520f7f12fb9d9bcabfddbef0467
Tan-Binh <phantanbinh0505@gmail.com>
2022-01-22 19:11:23 +0700

    start

---
+++ js/home.js
@@ -0,0 +1,31 @@
+var slideIndex = 0;
+
+showSlides_auto();
+
+function showSlides_auto() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides_auto, 5000); // Change image every 2 seconds
+}
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides_click(slideIndex += n);
+}
+
+function showSlides_click(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndex-1].style.display = "block";
+}
